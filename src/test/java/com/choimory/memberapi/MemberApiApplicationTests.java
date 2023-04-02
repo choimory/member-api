ed1c10ad5ebefecc93688f84a57c9a6e4718c26c
choimory <choimory@naver.com>
2023-04-02 11:00:11 +0900

    Initial commit

---
+++ src/test/java/com/choimory/memberapi/MemberApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.choimory.memberapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
