1f17fa0eb4b719925180dc263315f997942ea67c
choimory <choimory@naver.com>
2023-04-05 18:59:23 +0900

    feature: add member agreement test

---
+++ src/test/java/com/choimory/memberapi/member/entity/MemberAgreementTest.java
@@ -0,0 +1,17 @@
+package com.choimory.memberapi.member.entity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberAgreementTest {
+    @Test
+    public void 복합키_테스트(){
+        //TODO
+        //리포지토리 작성 후
+    }
+}
