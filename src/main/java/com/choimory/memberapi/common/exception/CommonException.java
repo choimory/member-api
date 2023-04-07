4b6806725cd1915d9b270a1b577f2ca38193e69b
choimory <choimory@naver.com>
2023-04-07 18:22:33 +0900

    feature: 공용 예외 클래스

---
+++ src/main/java/com/choimory/memberapi/common/exception/CommonException.java
@@ -0,0 +1,15 @@
+package com.choimory.memberapi.common.exception;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Builder
+@Getter
+@RequiredArgsConstructor
+public class CommonException extends RuntimeException {
+    private final HttpStatus httpStatus;
+    private final int status;
+    private final String message;
+}
