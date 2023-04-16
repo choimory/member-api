2d3e075dacc94bd0a0b09995590ef254b0abd723
choimory <choimory@naver.com>
2023-04-16 22:44:04 +0900

    test: 로그인 API 테스트

--- src/test/java/com/choimory/memberapi/member/controller/MemberControllerTest.java
+++ src/test/java/com/choimory/memberapi/member/controller/MemberControllerTest.java
@@ -1,6 +1,7 @@
 package com.choimory.memberapi.member.controller;
 
 import com.choimory.memberapi.config.SpringRestDocsConfig;
+import com.choimory.memberapi.member.data.request.RequestMemberLogin;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -22,6 +23,9 @@ import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.stream.Stream;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -49,21 +53,21 @@ class MemberControllerTest {
         /*given*/
         /*when*/
         // ResultActions when = mockMvc.perform(RestDocumentationRequestBuilders.get("/member/{identity}", identity) - MockMvcRequestBuilder.get()이 pathRequest 지원 안하는 버전일시 사용
-        ResultActions when = mockMvc.perform(MockMvcRequestBuilders.get("/member/{identity}", identity)
+        ResultActions when = mockMvc.perform(get("/member/{identity}", identity)
                 .accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON));
 
         /*then*/
         when.andDo(MockMvcResultHandlers.print());
         if(isSuccess){
-            when.andExpect(MockMvcResultMatchers.status().is(httpStatus.value()))
-                    .andExpect(MockMvcResultMatchers.jsonPath("member.identity").value(identity))
-                    .andExpect(MockMvcResultMatchers.jsonPath("member.password").doesNotExist());
+            when.andExpect(status().is(httpStatus.value()))
+                    .andExpect(jsonPath("member.identity").value(identity))
+                    .andExpect(jsonPath("member.password").doesNotExist());
         } else {
-            when.andExpect(MockMvcResultMatchers.status().is(httpStatus.value()))
-                    .andExpect(MockMvcResultMatchers.jsonPath("status").value(httpStatus.value()))
-                    .andExpect(MockMvcResultMatchers.jsonPath("message").value(httpStatus.getReasonPhrase()))
-                    .andExpect(MockMvcResultMatchers.jsonPath("member").doesNotExist());
+            when.andExpect(status().is(httpStatus.value()))
+                    .andExpect(jsonPath("status").value(httpStatus.value()))
+                    .andExpect(jsonPath("message").value(httpStatus.getReasonPhrase()))
+                    .andExpect(jsonPath("member").doesNotExist());
         }
     }
 
@@ -81,11 +85,31 @@ class MemberControllerTest {
         /*then*/
     }
 
-    @Test
-    void login() {
+    @DisplayName("로그인 테스트")
+    @ParameterizedTest
+    @MethodSource("loginMethodSource")
+    void login(final boolean isSuccess, final String identity, final String password, final HttpStatus httpStatus) throws Exception {
         /*given*/
+        RequestMemberLogin param = RequestMemberLogin.builder()
+                .identity(identity)
+                .password(password)
+                .build();
+
         /*when*/
+        ResultActions when = mockMvc.perform(post("/member/login")
+                .content(objectMapper.writeValueAsString(param))
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON));
+
         /*then*/
+        when.andDo(MockMvcResultHandlers.print());
+        if(isSuccess){
+            when.andExpect(status().is(httpStatus.value()))
+                    .andExpect(jsonPath("identity").value(identity))
+                    .andExpect(jsonPath("token").exists());
+        } else {
+            when.andExpect(status().is(httpStatus.value()));
+        }
     }
 
     @Test
@@ -109,4 +133,17 @@ class MemberControllerTest {
                 .build();
     }
 
+    static Stream<Arguments> loginMethodSource(){
+        return Stream.<Arguments>builder()
+                .add(Arguments.arguments(true, "choimory", "qwe123!@#", HttpStatus.OK))
+                .add(Arguments.arguments(false, "choimory", "qweqwe", HttpStatus.BAD_REQUEST))
+                .add(Arguments.arguments(false, "choimoryy", "qwe123!@#", HttpStatus.NOT_FOUND))
+                .add(Arguments.arguments(false, "cho imory", "qwe123!@#", HttpStatus.NOT_FOUND))
+                .add(Arguments.arguments(false, "choimory", "as dqwe123", HttpStatus.BAD_REQUEST))
+                .add(Arguments.arguments(false, "", "asdqwe123", HttpStatus.BAD_REQUEST))
+                .add(Arguments.arguments(false, "choimory", "", HttpStatus.BAD_REQUEST))
+                .add(Arguments.arguments(false, null, "asdqwe123", HttpStatus.BAD_REQUEST))
+                .add(Arguments.arguments(false, "choimory", null, HttpStatus.BAD_REQUEST))
+                .build();
+    }
 }
