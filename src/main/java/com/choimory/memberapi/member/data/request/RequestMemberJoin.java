97611fdc22fd989abe17ae3f4b99129fe39e64f7
choimory <choimory@naver.com>
2023-07-08 21:21:07 +0900

    feature: 회원가입 요청값

--- src/main/java/com/choimory/memberapi/member/data/request/RequestMemberJoin.java
+++ src/main/java/com/choimory/memberapi/member/data/request/RequestMemberJoin.java
@@ -1,4 +1,45 @@
 package com.choimory.memberapi.member.data.request;
 
+import com.choimory.memberapi.member.entity.MemberImage;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Getter
 public class RequestMemberJoin {
+    //아이디
+    @NotEmpty
+    @NotBlank
+    @Length(min = 8, max = 20)
+    private final String identity;
+
+    //비밀번호
+    @NotEmpty
+    @NotBlank
+    @Length(min = 8, max = 20)
+    private final String password;
+
+    //이메일
+    @Email
+    private final String email;
+
+    //자기소개
+    @Max(1000)
+    private final String profile;
+
+    //이미지
+    private final List<@Valid Image> images;
+
+    @RequiredArgsConstructor
+    @Getter
+    public static class Image{
+        private final MemberImage.Type type;
+        private final MultipartFile file;
+    }
 }
